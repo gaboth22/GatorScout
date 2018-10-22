cb4a5e78005a08d688fa5de38d266acc812c4611
Gabriel Tapizquent <gaboth22@gmail.com>
2018-10-22 18:33:57 -0400

    Latest fixes for main PCB

--- MSP432/Adc/Adc_Precision14.c
+++ MSP432/Adc/Adc_Precision14.c
@@ -24,26 +24,26 @@ I_Adc_t * Adc_Precision14_Init(void)
 {
     instance.api = &api;
     avgIndex = 0;
-    P4->SEL0 |= BIT7; // Set P4.7 as second peripheral usage - A6
-    P4->SEL1 |= BIT7; // Set P4.7 as second peripheral usage - A6
+    P8->SEL0 |= BIT6; // Set P8.6 as second peripheral usage - A19
+    P8->SEL1 |= BIT6; // Set P8.6 as second peripheral usage - A19
     NVIC->ISER[0] |= 1 << ((ADC14_IRQn) & 31);
     while(REFCTL0 & REFGENBUSY);
-    REF_A->CTL0 = REF_A_CTL0_OUT| REF_A_CTL0_VSEL_3 | REF_A_CTL0_ON;
-    ADC14->CTL0 = ADC14_CTL0_SHT0_7 | ADC14_CTL0_SSEL_0 | ADC14_CTL0_SHP | ADC14_CTL0_CONSEQ_0 | ADC14_CTL0_ON;
-    ADC14->CTL1 = ADC14_CTL1_RES_2 | (ADC14_CTL1_CSTARTADD_MASK & (6 << 16)) | ADC14_CTL1_REFBURST;
-    ADC14->MCTL[6] |= (ADC14_MCTLN_INCH_6 | ADC14_MCTLN_VRSEL_1); // Input A6, buffered reference - 2.5V
+    REF_A->CTL0 = REF_A_CTL0_OUT | REF_A_CTL0_VSEL_3 | REF_A_CTL0_ON;
+    ADC14->CTL0 = ADC14_CTL0_SHT0_6 | ADC14_CTL0_SSEL_0 | ADC14_CTL0_SHP | ADC14_CTL0_CONSEQ_0 | ADC14_CTL0_ON;
+    ADC14->CTL1 = ADC14_CTL1_RES_2 | (ADC14_CTL1_CSTARTADD_MASK & (19 << 16)) | ADC14_CTL1_REFBURST;
+    ADC14->MCTL[19] |= (ADC14_MCTLN_INCH_19 | ADC14_MCTLN_VRSEL_1); // Input A19, buffered reference - 2.5V
     ADC14->CTL0 |= (ADC14_CTL0_ENC | ADC14_CTL0_SC); // Start sampling and conversion
-    ADC14->IER0 = ADC14_IER0_IE6;
+    ADC14->IER0 = ADC14_IER0_IE19;
 
     return &instance;
 }
 
 void ADC14_IRQHandler(void)
 {
-    if(ADC14->IV == 0x18)
+    if(ADC14->IV == 0x32)
     {
-        ADC14->CLRIFGR0 = ADC14_CLRIFGR0_CLRIFG6;
-        runningAverageData[avgIndex] = (AdcCounts_t)ADC14->MEM[6];
+        ADC14->CLRIFGR0 = ADC14_CLRIFGR0_CLRIFG19;
+        runningAverageData[avgIndex] = (AdcCounts_t)ADC14->MEM[19];
         avgIndex++;
         avgIndex = avgIndex % RunningAvgSize;
         ADC14->CTL0 |= (ADC14_CTL0_ENC | ADC14_CTL0_SC);
--- MSP432/GpioGroup/GpioTable.h
+++ MSP432/GpioGroup/GpioTable.h
@@ -20,15 +20,15 @@ typedef enum
     HOW(GpioPwm4_P2B7,          8,  2, 7, GpioMode_Output) \
     HOW(GpioWheelEncoder1,      9,  3, 2, GpioMode_Input)  \
     HOW(GpioWheelEncoder2,      10, 3, 3, GpioMode_Input)  \
-    HOW(GpioIrSensorInput,      11, 4, 7, GpioMode_Input)  \
-    HOW(GpioUltraSonicEcho1,    12, 5, 0, GpioMode_Input)  \
-    HOW(GpioUltraSonicEcho2,    13, 5, 1, GpioMode_Input)  \
-    HOW(GpioSpiCs,              14, 6, 2, GpioMode_Output) \
-    HOW(GpioSpiClk,             15, 6, 3, GpioMode_Output) \
-    HOW(GpioSpiMosi,            16, 6, 4, GpioMode_Output) \
-    HOW(GpioSpiMiso,            17, 6, 5, GpioMode_Input)  \
-    HOW(GpioUltraSonicTrigger1, 18, 7, 1, GpioMode_Output) \
-    HOW(GpioUltraSonicTrigger2, 19, 7, 2, GpioMode_Output) \
+    HOW(GpioUltraSonicEcho1,    11, 5, 0, GpioMode_Input)  \
+    HOW(GpioUltraSonicEcho2,    12, 5, 1, GpioMode_Input)  \
+    HOW(GpioSpiCs,              13, 6, 2, GpioMode_Output) \
+    HOW(GpioSpiClk,             14, 6, 3, GpioMode_Output) \
+    HOW(GpioSpiMosi,            15, 6, 4, GpioMode_Output) \
+    HOW(GpioSpiMiso,            16, 6, 5, GpioMode_Input)  \
+    HOW(GpioUltraSonicTrigger1, 17, 7, 1, GpioMode_Output) \
+    HOW(GpioUltraSonicTrigger2, 18, 7, 2, GpioMode_Output) \
+    HOW(GpioIrSensorInput,      19, 8, 6, GpioMode_Input)  \
     HOW(GpioUartUsca3Rx,        20, 9, 6, GpioMode_Input)  \
     HOW(GpioUartUsca3Tx,        21, 9, 7, GpioMode_Output) \
 
